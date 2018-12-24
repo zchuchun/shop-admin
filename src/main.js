75be9ab5e328c5166e465ad4e260ebf8179b6ae7
zchuchun <2275953401@qq.com>
2018-12-24 16:01:32 +0800

    基本配置完成

---
+++ src/main.js
@@ -0,0 +1,24 @@
+
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+// 导入element-ui
+import ElementUI from 'element-ui'
+// 导入element-ui的样式文件
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 引入公共样式
+import '@/assets/base.css'
+
+// 使用element-ul插件
+Vue.use(ElementUI)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '@/components/Login'
+import Home from '@/components/Home'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: Login
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/home',
+      component: Home
+    }
+  ]
+})
